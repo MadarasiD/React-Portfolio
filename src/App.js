c59dd7f6116ff948ee912d767acbe0670948f638
MadarasiD <madarka0913@gmail.com>
2022-05-21 20:47:30 +0200

    App.js-ben lévő Route-hoz való file-ok létrehozás

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Router>
+        <Navbar />
+          <Routes>
+            <Route path="/" element={ <Home />} />
+            <Route path="/about" element={ <About />} />
+            <Route path="/portfolio" element={ <Portfolio />} />
+            <Route path="/contact" element={ <Contact />} />
+          </Routes>
+      </Router>
     </div>
   );
 }
