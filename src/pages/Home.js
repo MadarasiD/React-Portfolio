d120a9aaa712f1cf9fe36b6fc3590c98a232264a
MadarasiD <madarka0913@gmail.com>
2022-05-21 21:28:43 +0200

    home file img hozzáadása

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroImage from '../assets/Home.jpg'
+import {Link} from "react-router-dom"
+
+const Home = () => {
+  return (
+    <div className="home">
+        <div className="homecontainer">
+            <h1>Front End Developer</h1>
+            <p>I am a Front End Developer</p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
