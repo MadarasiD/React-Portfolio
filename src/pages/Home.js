6f4290197ff3126e4811c6e0081d215fa27fe038
MadarasiD <madarka0913@gmail.com>
2022-05-21 23:04:33 +0200

    home style beállítások

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,14 @@ import {Link} from "react-router-dom"
 
 const Home = () => {
   return (
-    <div className="home">
+    <div className="home" style={{backgroundImage: `url(${HeroImage})`}}>
         <div className="homecontainer">
             <h1>Front End Developer</h1>
             <p>I am a Front End Developer</p>
+
+            <Link to="/menu">
+                <button>gomb</button>
+            </Link>
         </div>
     </div>
   )
