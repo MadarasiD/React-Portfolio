ab86ce5088f1f1c19a51edc12b12366899d8ccfc
MadarasiD <madarka0913@gmail.com>
2022-05-22 18:57:16 +0200

    Home css kész

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import HeroImage from '../assets/Home.jpg'
+import HeroImage from '../assets/Home2.jpg'
+import "../styles/Home.css"
 import {Link} from "react-router-dom"
 
 const Home = () => {
