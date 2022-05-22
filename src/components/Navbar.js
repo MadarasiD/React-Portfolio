88f815e5c899937efc959274f88c3d7583c629a5
MadarasiD <madarka0913@gmail.com>
2022-05-22 19:10:31 +0200

    Navbar beállítása

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+import ReorderIcon from '@mui/icons-material/Reorder';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+
+      <div className="navbar-left">
+        <p>Madarasi Dániel</p>
+      </div>
+
+      <div className="navbar-right">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/portolio">Portfolio</Link>
+          <Link to="/contact">Contact</Link>
+
+          <button>
+              <ReorderIcon />
+          </button>
+      </div>
+
+
+    </div>
   )
 }
 
