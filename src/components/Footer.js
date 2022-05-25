57f6cddc2c482f56f3ab9d5e100a1f2070e4462c
MadarasiD <madarka0913@gmail.com>
2022-05-25 21:10:27 +0200

    social media beállítása

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,28 @@
 import { Facebook, GitHub, Instagram, LinkedIn, Twitter } from '@mui/icons-material'
 import React from 'react'
+import '../styles/Footer.css'
 
 const Footer = () => {
   return (
     <div className="footer">
 
         <div className="socialMedia">
+        <a rel="stylesheet" href="/" ><Instagram  /> <span>Instagram</span></a><br />
+          <Twitter /> <span>Twitter</span> <br /> 
+          <Facebook /> <span>Facebook</span> <br />
+          <LinkedIn /> <span>LinkedIn</span> <br />
+          <GitHub /> <span>GitHub</span>
+        </div>
 
-          <Instagram /><br /> <Twitter /><br /> <Facebook /><br /> <LinkedIn /><br /> <GitHub />
+
+        <div className="monogram">
+
+        <p>2022 &copy; Madarasi Dániel - Front End Developer</p>
 
         </div>
 
-        <p>2022 &copy; Madarasi Dániel - Front End Developer</p>
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 
     </div>
   )
